e82947fbebfd16b7911651a2300637c9c9d30849
Rikia Stewart <rikia@rikias-mbp.myfiosgateway.com>
2023-07-09 11:37:11 -0400

    update sessions file to import database relate variables

--- session.py
+++ session.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from local_settings import DB_PASSWORD
+from local_settings import DATABASE_PASSWORD
 from local_settings import DATABASE_ENDPOINT
 from local_settings import DATABASE_NAME
 from local_settings import USERNAME
 
 # create an engine
-engine = create_engine(f"mysql://{USERNAME}:{DB_PASSWORD}@{DATABASE_ENDPOINT}/{DATABASE_NAME}", echo=True)
+engine = create_engine(f"mysql://{USERNAME}:{DATABASE_PASSWORD}@{DATABASE_ENDPOINT}/{DATABASE_NAME}", echo=True)
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
