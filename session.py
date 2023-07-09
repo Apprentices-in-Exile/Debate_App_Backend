0e7f3863bb9707255b63e2d79d7e909c055ee7cc
Rikia Stewart <rikia@rikias-mbp.myfiosgateway.com>
2023-07-09 11:46:37 -0400

    clean up white space

--- session.py
+++ session.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from local_settings import DATABASE_PASSWORD
 from local_settings import DATABASE_ENDPOINT
 from local_settings import DATABASE_NAME
